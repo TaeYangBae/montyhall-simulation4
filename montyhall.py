ed0bdcc87e0ed08b6be821f70c34300e21c3f92d
TaeYangBae <qoxodid88@naver.com>
2023-05-02 16:50:01 +0900

    "feat: last code "

--- montyhall.py
+++ montyhall.py
@@ -1,4 +1,3 @@
-
 import random 
 
 door = ['a', 'b', 'c']
@@ -18,4 +17,13 @@ for _ in range(trial):
 
     com = random.sample(empty_door, 1)[0]
 
+    if player_choice == car:
+        no_change += 1
+
+    empty_door.remove(com)
+
+    if not empty_door:
+        change +=1
+print("선택을 바꾸지 않은 경우:", no_change/trial,"%")
+print("선택을 바꾼 경우:", change/trial,"%")
 
